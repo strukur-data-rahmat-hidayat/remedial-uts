ffd082078371d5be7bb3a444ab5fe6a02725a9a2
rahmathidayat1 <49294613+rahmathidayat1@users.noreply.github.com>
2019-05-12 14:00:14 +0800

    Create uts1.cpp

---
+++ uts1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+string a,c,d;
+int b;
+cout<<"Nama : ";
+cin>>a;
+cout<<"Nim : ";
+cin>>b;
+cout<<"Kelas : ";
+cin>>c;
+cout<<"Prodi :";
+cin>>d;
+cout<<"\n======================\n";
+cout<<"Nama : "<<a<<endl;
+cout<<"Nim : "<<b<<endl;
+cout<<"Kelas : "<<c<<endl;
+cout<<"Prodi : "<<d<<endl;
+}
